142dbeb3346215a09b8e2fa27fe8532ef1ec10fb
Dhanashri Chavan <dhanachavan@github.com>
2025-09-17 09:55:15 +0530

    feat: add HelloWorldComponent and its unit tests

---
+++ portal/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HelloWorldComponent } from './hello-world.component';
+
+describe('HelloWorldComponent', () => {
+	let component: HelloWorldComponent;
+	let fixture: ComponentFixture<HelloWorldComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ HelloWorldComponent ]
+		})
+		.compileComponents();
+	});
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(HelloWorldComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should have a title', () => {
+		component.title = 'Hello World';
+		expect(component.title).toEqual('Hello World');
+	});
+});
